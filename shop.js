7086349e9fde755600817cd2722cf101913dfbe6
Julani7 <53162801+Julani7@users.noreply.github.com>
2020-05-01 22:55:29 -0400

    Complete list and Functions

---
+++ shop.js
@@ -0,0 +1,29 @@
+$(function run() {
+    $('#js-shopping-list-form').submit(function (event) {
+        event.preventDefault();
+        Value = $('input[name="shopping-list-entry"]').val();
+
+        $('.shopping-list').append(`<li>
+            <span class="shopping-item">${Value}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`);
+        $('input[name="shopping-list-entry"]').val("");
+
+    })
+    $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+        $(this).closest('li').remove();
+    })
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    })
+})
+
+$(run);
